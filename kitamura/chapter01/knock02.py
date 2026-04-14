935aaaef6f6c681255edc052ac75e059526f44e5
arisa-1814 <arisa@k03.itscom.net>
2026-04-14 18:59:38 +0900

    done

---
+++ kitamura/chapter01/knock02.py
@@ -0,0 +1,6 @@
+tx = "stressed"
+answer = ""
+for i in range (-1, -8, -1):
+    answer += tx[i]
+
+print(answer)
---
+++ kitamura/chapter01/knock03.py
@@ -0,0 +1,5 @@
+tx = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+list = []
+
+tx = tx.split()
+print(tx)
